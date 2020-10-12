a43be979b46017983789cd5021603e6520bbc35b
SK9604 <tjsrud428@gmail.com>
2020-10-12 15:53:10 +0900

    무중단 배포

--- src/main/java/com/sk/book/springboot/web/ProfileController.java
+++ src/main/java/com/sk/book/springboot/web/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
@@ -15,12 +16,14 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String profile() {
-        List<String> profiles = Arrays.asList(env.getActiveProfiles()); // 현재 실행 중인 ActiveProfile을 모두 가져온다. 즉, real, oauth, real-db 등이 활성화되어 있다면(active) 3개가 모두 담겨 있다
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-        String defaultProfile = profiles.isEmpty()? "default": profiles.get(0);
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
                 .orElse(defaultProfile);
     }
 }
+
